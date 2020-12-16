07b760e5e997bb5096c366e1d3b918eac29801ab
oosman <oosman3@yahoo.com>
2020-12-16 03:32:34 +0000

    compiles for x86_64 linux

--- build.sh
+++ build.sh
@@ -4,33 +4,37 @@ set -xe
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-if [ ! -d libedgetpu ]; then
-   git clone https://github.com/jambamamba/libedgetpu.git
-fi
-pushd libedgetpu
-if [ ! -d tensorflow ]; then
-   git clone https://github.com/jambamamba/tensorflow.git
-fi
 if [ ! -d libusb ]; then
    git clone https://github.com/jambamamba/libusb.git
 fi
+if [ ! -d systemd ]; then
+   git clone https://github.com/systemd/systemd.git
+fi
+if [ ! -d libedgetpu ]; then
+   git clone https://github.com/jambamamba/libedgetpu.git
+fi
+if [ ! -d tensorflow ]; then
+   git clone https://github.com/jambamamba/tensorflow.git
+fi
 
 pushd libusb
-git checkout x86_64
-./autogen.sh
-mkdir -p build
-pushd build
-cmake ..
-make -j$(getconf _NPROCESSORS_ONLN)
-popd
+	git checkout x86_64
+	./autogen.sh
+	mkdir -p build
+	pushd build
+		cmake ..
+		make -j$(getconf _NPROCESSORS_ONLN)
+	popd
 popd
 
-git checkout x86_64
-mkdir -p /tmp/.bazel
-TEST_TMPDIR=/tmp/.bazel make
-rm -f bazel-build
-bazelbuild=$(TEST_TMPDIR=/tmp/.bazel bazel info --experimental_repo_remote_exec 2>/dev/null | grep "execution_root:" | cut -d " " -f 2 | xargs dirname | xargs dirname)
-ln -s $bazelbuild bazel-build
+pushd libedgetpu
+	git checkout x86_64
+	if [ ! -d tensorflow ]; then ln -s ../tensorflow tensorflow; fi
+	mkdir -p /tmp/.bazel
+	TEST_TMPDIR=/tmp/.bazel make
+	rm -f bazel-build
+	bazelbuild=$(TEST_TMPDIR=/tmp/.bazel bazel info --experimental_repo_remote_exec 2>/dev/null | grep "execution_root:" | cut -d " " -f 2 | xargs dirname | xargs dirname)
+	ln -s $bazelbuild bazel-build
 popd
 
 mkdir -p build
