13896aaf85f2840e694e4b44587338392bb780c2
oosman <oosman3@yahoo.com>
2020-12-21 17:24:24 +0000

    wip

--- build.sh
+++ build.sh
@@ -4,92 +4,168 @@ set -xe
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 
-PI_TOOLCHAIN_ROOT_DIR=${HOME}/${DOCKERUSER}/.leila/toolchains/rpi
+function parseArgs()
+{
+  for change in $@; do
+      set -- `echo $change | tr '=' ' '`
+      echo "variable name == $1  and variable value == $2"
+      #can assign value to a variable like below
+      eval $1=$2;
+  done
+}
 
-if [ ! -d libusb ]; then
-   git clone https://github.com/jambamamba/libusb.git
-fi
-if [ ! -d systemd ]; then
-   git clone https://github.com/systemd/systemd.git
-fi
-if [ ! -d libedgetpu ]; then
-   git clone https://github.com/jambamamba/libedgetpu.git
-fi
-if [ ! -d tensorflow ]; then
-   git clone https://github.com/jambamamba/tensorflow.git
-fi
-
-pushd libusb
-	git checkout x86_64
-	if [ ! -f config.h ]; then 
-		./autogen.sh
+function cloneRepos()
+{
+	if [ ! -d libusb ]; then
+	   git clone https://github.com/jambamamba/libusb.git
 	fi
-	mkdir -p build
-	pushd build
-		rm -fr *
-		cmake ..
-		make -j$(getconf _NPROCESSORS_ONLN)
-	popd
-	
-	if [ -d $PI_TOOLCHAIN_ROOT_DIR ]; then
-	mkdir -p buildpi
-	pushd buildpi
-		rm -fr *
-		cmake -DCMAKE_TOOLCHAIN_FILE=$PI_TOOLCHAIN_ROOT_DIR/Toolchain-RaspberryPi.cmake ../
-		make -j$(getconf _NPROCESSORS_ONLN)
-	popd
+	if [ ! -d systemd ]; then
+	   git clone https://github.com/systemd/systemd.git
 	fi
+	if [ ! -d libedgetpu ]; then
+	   git clone https://github.com/jambamamba/libedgetpu.git
+	fi
+	if [ ! -d tensorflow ]; then
+	   git clone https://github.com/jambamamba/tensorflow.git
+	fi
+}
+
+function buildlibusb()
+{
+	parseArgs "$@"
 	
-popd
+	pushd libusb
+		git checkout x86_64 #works for both host and rpi!
+		
+		if [ "$arch" == "host" ]; then
+			if [ ! -f config.h ]; then 
+				./autogen.sh
+			fi
+			mkdir -p build
+			pushd build
+				rm -fr *
+				cmake ..
+				make -j$(getconf _NPROCESSORS_ONLN)
+			popd
+		elif [ "$arch" == "rpi" ]; then
+			mkdir -p buildpi
+			pushd buildpi
+				rm -fr *
+				cmake -DCMAKE_TOOLCHAIN_FILE=$PI_TOOLCHAIN_ROOT_DIR/Toolchain-RaspberryPi.cmake ../
+				make -j$(getconf _NPROCESSORS_ONLN)
+			popd
+		fi
+		
+	popd
+}
 
-pushd libedgetpu
-	git checkout x86_64
-	if [ ! -d tensorflow ]; then ln -s ../tensorflow tensorflow; fi
-	mkdir -p /tmp/.bazel
-	TEST_TMPDIR=/tmp/.bazel make
-	rm -f bazel-build
-	bazelbuild=$(TEST_TMPDIR=/tmp/.bazel bazel info --experimental_repo_remote_exec 2>/dev/null | grep "execution_root:" | cut -d " " -f 2 | xargs dirname | xargs dirname)
-	ln -s $bazelbuild bazel-build
-popd
-
-mkdir -p build
-	pushd build
-	rm -fr *
-	cmake ..
-	make -j$(getconf _NPROCESSORS_ONLN)
-popd
-
-if [ -d $PI_TOOLCHAIN_ROOT_DIR ]; then
-mkdir -p buildpi
-	pushd buildpi
-	rm -fr *
-	cmake -DCMAKE_TOOLCHAIN_FILE=$PI_TOOLCHAIN_ROOT_DIR/Toolchain-RaspberryPi.cmake ../
-	make -j$(getconf _NPROCESSORS_ONLN)
-popd
-fi
-
-if [ ! -f /tmp/mobilenet_v1_1.0_224_quant_edgetpu.tflite ]; then
-   pushd /tmp
-   wget https://github.com/google-coral/edgetpu/raw/master/test_data/mobilenet_v1_1.0_224_quant_edgetpu.tflite
-   popd
-fi
-
-if [ -d $PI_TOOLCHAIN_ROOT_DIR ]; then
-	mkdir -p ./buildpi/stripped
-	cp -f ./buildpi/libedgetpu/libedgetpu.so ./buildpi/stripped/
-	cp -f ./buildpi/libusb/libusb.so ./buildpi/stripped/
-	cp -f ./buildpi/minimal ./buildpi/stripped/
-	chrpath -r '$ORIGIN/.' ./buildpi/stripped/minimal
-
-	$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/minimal
-	$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/libedgetpu.so
-	$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/libusb.so
-
-	#scp ./buildpi/stripped/* pi@192.168.1.26:/tmp
-	#scp /tmp/mobilenet_v1_1.0_224_quant_edgetpu.tflite pi@192.168.1.26:/tmp/
+function buildlibedgetpu()
+{
+	parseArgs "$@"
 	
-	file ./buildpi/minimal | cowsay
-else
-	file ./build/minimal | cowsay
-fi
+	pushd libedgetpu
+		if [ "$arch" == "host" ]; then
+			git checkout x86_64
+		elif [ "$arch" == "rpi" ]; then
+			git checkout raspi0
+		fi
+	
+		if [ ! -d tensorflow ]; then ln -s ../tensorflow tensorflow; fi
+		mkdir -p /tmp/.bazel
+		TEST_TMPDIR=/tmp/.bazel PI_TOOLCHAIN_ROOT_DIR=${PI_TOOLCHAIN_ROOT_DIR} make
+		rm -f bazel-build
+		bazelbuild=$(TEST_TMPDIR=/tmp/.bazel bazel info --experimental_repo_remote_exec 2>/dev/null | grep "execution_root:" | cut -d " " -f 2 | xargs dirname | xargs dirname)
+		ln -sf $bazelbuild bazel-build
+	popd
+
+}
+
+function buildProject()
+{
+	parseArgs "$@"
+	
+	if [ "$arch" == "host" ]; then
+		mkdir -p build
+		pushd build
+			rm -fr *
+			cmake ..
+			make -j$(getconf _NPROCESSORS_ONLN)
+		popd
+	elif [ "$arch" == "rpi" ]; then
+		mkdir -p buildpi
+			pushd buildpi
+			rm -fr *
+			cmake -DCMAKE_TOOLCHAIN_FILE=$PI_TOOLCHAIN_ROOT_DIR/Toolchain-RaspberryPi.cmake ../
+			make -j$(getconf _NPROCESSORS_ONLN)
+		popd
+	else
+		echo "invalid arch supplied, should be  arch=host or arch=rpi"
+		exit -1
+	fi
+}
+
+function downloadTfModel()
+{
+	if [ ! -f /tmp/mobilenet_v1_1.0_224_quant_edgetpu.tflite ]; then
+	   pushd /tmp
+	   wget https://github.com/google-coral/edgetpu/raw/master/test_data/mobilenet_v1_1.0_224_quant_edgetpu.tflite
+	   popd
+	fi
+}
+
+function stripCrossCompiledBinaries()
+{
+	parseArgs "$@"
+	if [ "$arch" == "rpi" ]; then
+		mkdir -p ./buildpi/stripped
+		cp -f ./buildpi/libedgetpu/libedgetpu.so ./buildpi/stripped/
+		cp -f ./buildpi/libusb/libusb.so ./buildpi/stripped/
+		cp -f ./buildpi/minimal ./buildpi/stripped/
+		chrpath -r '$ORIGIN/.' ./buildpi/stripped/minimal
+
+		$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/minimal
+		$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/libedgetpu.so
+		$PI_TOOLCHAIN_ROOT_DIR/x-tools/arm-rpi-linux-gnueabihf/bin/arm-rpi-linux-gnueabihf-strip ./buildpi/stripped/libusb.so
+
+		#scp ./buildpi/stripped/* pi@192.168.1.26:/tmp
+		#scp /tmp/mobilenet_v1_1.0_224_quant_edgetpu.tflite pi@192.168.1.26:/tmp/
+	fi
+}
+
+function showResult()
+{
+	parseArgs "$@"
+	if [ "$arch" == "rpi" ]; then
+		file ./buildpi/minimal | cowsay
+	else
+		file ./build/minimal | cowsay
+	fi
+}
+
+function main()
+{
+	parseArgs "$@"
+	
+	if [ "$arch" == "" ]; then
+		while true; do
+		    read -p "Do you wish to build for host \n(next time try ./build.sh arch=host or ./build.sh arch=rpi)? " yn
+		    case $yn in
+			[Yy]* ) arch="host"; break;;
+			[Nn]* ) arch="rpi"; break;;
+			* ) echo "Please answer yes or no.";;
+		    esac
+		done
+	fi
+
+	PI_TOOLCHAIN_ROOT_DIR=${HOME}/${DOCKERUSER}/.leila/toolchains/rpi
+	cloneRepos
+	buildlibusb arch=$arch PI_TOOLCHAIN_ROOT_DIR=${PI_TOOLCHAIN_ROOT_DIR}
+	buildlibedgetpu arch=$arch PI_TOOLCHAIN_ROOT_DIR=${PI_TOOLCHAIN_ROOT_DIR}
+	buildProject arch=$arch PI_TOOLCHAIN_ROOT_DIR=${PI_TOOLCHAIN_ROOT_DIR}
+	downloadTfModel
+	stripCrossCompiledBinaries
+	showResult arch=$arch 
+}
+
+main "$@"
 
